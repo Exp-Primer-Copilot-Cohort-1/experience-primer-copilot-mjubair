248b673c07b4e8a2d441d9425bab81488975c7a9
Mohamed Jubair <53852702+mjubair@users.noreply.github.com>
2023-09-23 19:21:01 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,61 +1,24 @@
-// create web server 
-// 1. create web server object
-var http = require('http');
-// 2. create web server
-var server = http.createServer(function (req, res) {
-  // 3. set content-type
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  // 4. send response
-  res.end('<h1>Hello World!</h1>');
+// Create a web server
+// To run: node comment.js
+// To test: curl http://localhost:3000
+
+// Load the express module
+var express = require('express');
+
+// Create a new web server
+var app = express();
+
+// Define a route for the root
+app.get('/', function(req, res) {
+  res.send('Hello World');
 });
-// 5. listen to port
-server.listen(3000, function () {
-  console.log('Server started at port 3000');
+
+// Define a route for the comment
+app.get('/comment', function(req, res) {
+  res.send('Hello Comment');
 });
-// 6. run node app.js
-// 7. open browser and go to http://localhost:3000
-// 8. install nodemon globally
-// npm install -g nodemon
-// 9. run nodemon app.js
-// 10. open browser and go to http://localhost:3000
-// 11. add a route
-// 12. create a route object
-var route = {
-  home: function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Welcome Home!</h1>');
-  },
-  login: function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Please Login</h1>');
-  }
-};
-// 13. create a route function
-function routeFn(req, res) {
-  var url = req.url;
-  switch (url) {
-    case '/':
-      route.home(req, res);
-      break;
-    case '/login':
-      route.login(req, res);
-      break;
-    default:
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('<h1>Page not found</h1>');
-      break;
-  }
-}
-// 14. create web server object
-var http = require('http');
-// 15. create web server
-var server = http.createServer(routeFn);
-// 16. listen to port
-server.listen(3000, function () {
-  console.log('Server started at port 3000');
+
+// Start listening on port 3000
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
 });
-// 17. run nodemon app.js
-// 18. open browser and go to http://localhost:3000
-// 19. open browser and go to http://localhost:3000/login
-// 20. open browser and go to http://localhost:3000/abc
-// 21.
